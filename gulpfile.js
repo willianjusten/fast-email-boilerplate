f4a31cee7ddfe4ccba921d05171313254506cb0c
willianjusten <willianjustenqui@gmail.com>
2015-01-14 09:30:23 -0200

    Add browser-sync to workflow

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,13 @@
-var gulp      = require('gulp'),
-    plumber   = require('gulp-plumber'),
-    jade      = require('gulp-jade'),
-    stylus    = require('gulp-stylus'),
-    imagemin  = require('gulp-imagemin'),
-    prefixer  = require('autoprefixer-stylus'),
-    premailer = require('gulp-premailer');
+var gulp        = require('gulp'),
+    plumber     = require('gulp-plumber'),
+    jade        = require('gulp-jade'),
+    stylus      = require('gulp-stylus'),
+    imagemin    = require('gulp-imagemin'),
+    prefixer    = require('autoprefixer-stylus'),
+    browserSync = require('browser-sync'),
+    premailer   = require('gulp-premailer');
 
-
+// Jade Task => send to step
 gulp.task('jade', function(){
     return gulp.src('src/templates/*.jade')
         .pipe(plumber())
@@ -14,6 +15,7 @@ gulp.task('jade', function(){
         .pipe(gulp.dest('src/step/'))
 });
 
+// Stylus Task => send to step
 gulp.task('stylus', function(){
         gulp.src('src/styl/main.styl')
         .pipe(plumber())
@@ -24,6 +26,7 @@ gulp.task('stylus', function(){
         .pipe(gulp.dest('src/step/'))
 });
 
+// Imagemin Task => send to build
 gulp.task('imagemin', function() {
     return gulp.src('src/img/**/*')
         .pipe(plumber())
@@ -31,16 +34,31 @@ gulp.task('imagemin', function() {
         .pipe(gulp.dest('build/img'));
 });
 
+// Premailer Task => read from step and send to build
 gulp.task('premailer', function () {
     gulp.src('src/step/*.html')
         .pipe(premailer())
         .pipe(gulp.dest('build/'));
 });
 
+// Watch Task
 gulp.task('watch', function(){
     gulp.watch('src/templates/**/*.jade', ['jade', 'premailer']);
     gulp.watch('src/styl/**/*.styl', ['stylus','premailer']);
     gulp.watch('src/img/**/*.{jpg,png,gif}', ['imagemin']);
 });
 
-gulp.task('default', ['jade', 'stylus','imagemin', 'premailer', 'watch']);
+gulp.task('browser-sync', function () {
+   var files = [
+      'build/**/*.html',
+      'build/img/**/*',
+   ];
+
+   browserSync.init(files, {
+      server: {
+         baseDir: './build/'
+      }
+   });
+});
+
+gulp.task('default', ['jade', 'stylus','imagemin', 'premailer', 'watch', 'browser-sync']);
